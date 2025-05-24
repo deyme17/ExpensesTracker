141062581f507e8771baad65bf431ca6b0d9f6f1
dmytro <dmytro19022006@gmail.com>
2025-05-24 22:09:03 +0300

    db point

--- server/database/db.py
+++ server/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -10,4 +10,3 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-print(f"DSN (repr): {repr(DATABASE_URL)}")

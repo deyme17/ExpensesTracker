723db07e5e88971bed22a7c808022090d64ddc36
dmytro <dmytro19022006@gmail.com>
2025-05-26 13:04:46 +0300

    ping

--- server/app.py
+++ server/app.py
@@ -2,12 +2,15 @@ from server.api.auth_routes import auth_bp
 from flask import Flask
 from flask_cors import CORS
 from server.api.endpoints import api
+from server.api.base import ping_bp
+
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.register_blueprint(api)
     app.register_blueprint(auth_bp) 
+    app.register_blueprint(ping_bp, url_prefix="/api")
     return app
 
 app = create_app()

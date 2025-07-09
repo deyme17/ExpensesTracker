a590779e8c31877fa98b7a51afcef0a25e93c1b7
dmytro <dmytro19022006@gmail.com>
2025-07-09 13:19:06 +0300

    stores for prev state of filter/sort

---
+++ app/utils/state_savers.py
@@ -0,0 +1,37 @@
+from app.utils.constants import ALL, DATE_FIELD
+from datetime import datetime, timedelta
+
+class FilterState:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.min_amount = "0"
+        self.max_amount = "1000000"
+        self.start_date = datetime.now() - timedelta(days=365)
+        self.end_date = datetime.now()
+        self.type_selected = ALL
+        self.category_selected = ALL
+        self.payment_selected = ALL
+
+    def to_dict(self):
+        return {
+            "min_amount": self.min_amount,
+            "max_amount": self.max_amount,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "type_selected": self.type_selected,
+            "category_selected": self.category_selected,
+            "payment_selected": self.payment_selected,
+        }
+
+class SortState:
+    def __init__(self):
+        self.selected_field = DATE_FIELD
+        self.ascending = True
+
+    def to_dict(self):
+        return {
+            "selected_field": self.selected_field,
+            "ascending": self.ascending
+        }

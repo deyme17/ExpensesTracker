3b44011d58a3af8b91acb6fea5566e45ada41b5f
dmytro <dmytro19022006@gmail.com>
2025-07-09 14:32:17 +0300

    anot matedata/fix

--- app/controllers/metadata_controller.py
+++ app/controllers/metadata_controller.py
@@ -11,18 +11,18 @@ class MetaDataController:
         self._currencies_cache = None
         self._categories_cache = None
 
-    def get_currency_list(self, force_refresh=False):
+    def get_currencies(self, force_refresh: bool = False):
         if self._currencies_cache is None or force_refresh:
-            self._currencies_cache = self.currency_service.get_currency_list()
+            self._currencies_cache = self.currency_service.get_currencies()
         return self._currencies_cache
 
-    def get_currency_name_by_code(self, code):
-        return next((c.name for c in self.get_currency_list() if c.currency_code == code), str(code))
+    def get_currency_name_by_code(self, code: int):
+        return next((c.name for c in self.get_currencies() if c.currency_code == code), str(code))
 
-    def get_category_list(self, force_refresh=False):
+    def get_categories(self, force_refresh: bool = False):
         if self._categories_cache is None or force_refresh:
             self._categories_cache = self.category_service.get_categories()
         return self._categories_cache
 
-    def get_category_name_by_mcc(self, mcc):
-        return next((c.name for c in self.get_category_list() if c.mcc_code == mcc), str(mcc))
+    def get_category_name_by_mcc(self, mcc: int):
+        return next((c.name for c in self.get_categories() if c.mcc_code == mcc), str(mcc))

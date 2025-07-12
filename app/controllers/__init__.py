35ac194e94e48266598b83f0c82c87c6538e82e0
dmytro <dmytro19022006@gmail.com>
2025-07-13 01:01:35 +0300

    init

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -2,3 +2,4 @@ from app.controllers.analytics_controller import AnalyticsController
 from app.controllers.auth_controller import AuthController
 from app.controllers.transaction_controller import TransactionController 
 from app.controllers.metadata_controller import MetaDataController 
+from app.controllers.graph_controller import GraphController 

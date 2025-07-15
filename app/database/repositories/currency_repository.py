16625ef367fc533c15e207964e4e7f19c851f52d
dmytro <dmytro19022006@gmail.com>
2025-07-15 22:39:33 +0300

    currency repository

--- app/database/repositories/currency_repository.py
+++ app/database/repositories/currency_repository.py
@@ -1,5 +1,4 @@
 from sqlalchemy.orm import Session
-from typing import Optional
 
 from app.database.repositories.base_repository import BaseRepository
 from app.database.orm_models.currency import CurrencyORM
@@ -9,3 +8,20 @@ from app.models.currency import Currency
 class CurrencyRepository(BaseRepository[CurrencyORM]):
     def __init__(self, session: Session):
         super().__init__(session, CurrencyORM)
+
+    def save_currencies(self, currencies: list[Currency]):
+        for cat in currencies:
+            orm = CurrencyORM(
+                currency_code=cat.currency_code,
+                name=cat.name    
+            )
+            self.session.merge(orm)
+        self.session.commit()
+
+    def get_currencies(self) -> list[Currency]:
+        orm_currencies = self.session.query(CurrencyORM).all()
+        currencies = [Currency(
+                currency_code=cat.currency_code,
+                name=cat.name    
+            ) for cat in orm_currencies]
+        return currencies

d682a48f0b204533706f8d56c2d8edb883f0e630
dmytro <dmytro19022006@gmail.com>
2025-07-15 22:37:29 +0300

    category repository

--- app/database/repositories/category_repository.py
+++ app/database/repositories/category_repository.py
@@ -9,3 +9,20 @@ from app.models.category import Category
 class CategoryRepository(BaseRepository[CategoryORM]):
     def __init__(self, session: Session):
         super().__init__(session, CategoryORM)
+
+    def save_categories(self, categories: list[Category]):
+        for cat in categories:
+            orm = CategoryORM(
+                mcc_code=cat.mcc_code,
+                name=cat.name    
+            )
+            self.session.merge(orm)
+        self.session.commit()
+
+    def get_categories(self) -> list[Category]:
+        orm_categories = self.session.query(CategoryORM).all()
+        categories = [Category(
+                mcc_code=cat.mcc_code,
+                name=cat.name    
+            ) for cat in orm_categories]
+        return categories

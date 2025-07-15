bffc35894a1d3cab072bd934e617e155de8f56af
dmytro <dmytro19022006@gmail.com>
2025-07-16 02:04:28 +0300

    FUCK ENUM

--- app/database/orm_models/transaction.py
+++ app/database/orm_models/transaction.py
@@ -3,14 +3,7 @@ from sqlalchemy.sql import func
 import enum
 
 from app.database.orm_models.base_orm import Base
-from app.utils.constants import INCOME, EXPENSE, CARD, CASH, DEFAULT_CURRENCY_CODE, DEFAULT_MCC
-
-class TransactionType(enum.Enum):
-    EXPENSE = EXPENSE
-    INCOME  = INCOME
-class PaymentMethod(enum.Enum):
-    CARD = CARD
-    CASH = CASH
+from app.utils.constants import CARD, DEFAULT_CURRENCY_CODE, DEFAULT_MCC
 
 
 class TransactionORM(Base):
@@ -21,19 +14,11 @@ class TransactionORM(Base):
     amount = Column(Float, default=0.0)
     date = Column(DateTime, default=func.now())
     account_id = Column(String, nullable=False)
-    type = Column(
-        Enum(TransactionType, name="transaction_type"),
-        default=TransactionType.EXPENSE,
-        nullable=False
-    )
+    type = Column(String, nullable=False)
     mcc_code = Column(Integer, default=DEFAULT_MCC)
     currency_code = Column(Integer, default=DEFAULT_CURRENCY_CODE)
     description = Column(String, default="No description")
-    payment_method = Column(
-        Enum(PaymentMethod, name="payment_method"),
-        default=PaymentMethod.CARD,
-        nullable=False
-    )
+    payment_method = Column(String, nullable=False, default=CARD)
     cashback = Column(Float, default=0.0)
     commission = Column(Float, default=0.0)
     is_synced = Column(Boolean, default=False)

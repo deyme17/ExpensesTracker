3f99bbe58308a3aff4bcc7fb1da6a506acb6595b
dmytro <dmytro19022006@gmail.com>
2025-07-17 00:55:14 +0300

    prepare for webhook

--- server/services/__init__.py
+++ server/services/__init__.py
@@ -3,7 +3,7 @@ from server.services.auth_service import AuthService
 from server.services.bank_services.monobank_service import MonobankService
 from server.services.bank_sync_service import BankSyncService
 
-from server.services import user_service, account_service, transaction_service, category_service
+from server.services.crud import user_service, account_service, transaction_service, category_service
 
 bank_sync_service = BankSyncService(
     account_service=account_service,
@@ -15,9 +15,3 @@ auth_service = AuthService(
     bank_service_cls=MonobankService,
     bank_sync_service=bank_sync_service
 )
-
-
-# WebHook
-from .webhook_service import WebHookService
-
-webhook_service = WebHookService()

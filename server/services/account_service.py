11a4937265013be887f658db1cac1f3b5cea8b00
dmytro <dmytro19022006@gmail.com>
2025-07-16 14:28:56 +0300

    acc service improvment

--- server/services/account_service.py
+++ server/services/account_service.py
@@ -2,13 +2,44 @@ from server.database.repositories.account_repository import AccountRepository
 
 
 class AccountService:
-    def __init__(self):
-        self.repo = AccountRepository()
+    """
+    Service layer for account operations.
+    Args:
+        repository: AccountRepository instance for data access
+    """
+    def __init__(self, repository):
+        self.repo = repository
 
-    def create(self, data):
+    def create(self, data: dict):
+        """
+        Creates a new account.
+        Args:
+            data: Account data dictionary
+        Returns:
+            Created account object
+        """
         return self.repo.create(data)
+    
+    def update_balance(self, account_id: str, val: float):
+        """
+        Updates account balance.
+        Args:
+            account_id: ID of account to update
+            val: New balance value
+        Returns:
+            Updated account object
+        """
+        return self.repo.update(account_id, val)
 
-    def get_by_user_id(self, user_id):
+    def get_by_user_id(self, user_id: str) -> list[dict]:
+        """
+        Gets all accounts for specified user.
+        Args:
+            user_id: User ID to filter accounts
+        Returns:
+            List of account dictionaries
+        """
         return [a.to_dict() for a in self.repo.get_by_user_id(user_id)]
 
-account_service = AccountService()
+
+account_service = AccountService(repository=AccountRepository())

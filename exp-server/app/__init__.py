45702577b25591e4799c71acd06a84eb9109ae85
dmytro <dmytro19022006@gmail.com>
2025-07-23 16:04:39 +0300

    prefix status

--- exp-server/app/__init__.py
+++ exp-server/app/__init__.py
@@ -5,7 +5,7 @@ from app.routes.main_routes import api
 from app.routes.webhook_routes import mono_webhook_bp
 from app.routes.status import status_bp, ping_bp
 
-app.register_blueprint(status_bp)
+app.register_blueprint(status_bp, url_prefix="/status")
 app.register_blueprint(api)
 app.register_blueprint(auth_bp)
 app.register_blueprint(ping_bp, url_prefix="/api")

4fe15b64ef5f8e0c7cc79409e31473be371194ac
dmytro <dmytro19022006@gmail.com>
2025-07-25 15:19:45 +0300

    type fix

--- exp-server/app/utils/security.py
+++ exp-server/app/utils/security.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY", "super-secret228")
 ALGORITHM = os.getenv("ALGORITHM", "H256")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60*24)
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60*24))
 
 def hash_password(password: str) -> str:
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
